50bc4fdd69559bfbc2c580479933b0bcf8cb2080
hoctthe170098 <hoctthe170098@fpt.edu.vn>
2024-01-04 16:40:14 +0700

    lesson 1
    done

---
+++ WebAPI1/WebAPI1/Controllers/ProductController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.NetworkInformation;
+using WebAPI1.Models;
+
+namespace WebAPI1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public static List<Product> Products = new List<Product>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(Products);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            try
+            {
+                var product = Products.SingleOrDefault(p => p.MaHangHoa == Guid.Parse(id));
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                return Ok(product);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        [HttpPost]
+        public IActionResult Create(ProductVM productVM)
+        {
+            var product = new Product
+            {
+                MaHangHoa = Guid.NewGuid(),
+                Name = productVM.Name,
+                DonGia = productVM.DonGia,
+            };
+            Products.Add(product);
+            return Ok(new
+            {
+                Success = true,
+                Data = product
+            });
+        }
+        [HttpPut("{id}")]
+        public IActionResult Edit(string id, Product productEdit)
+        {
+            try
+            {
+                var product = Products.SingleOrDefault(p => p.MaHangHoa == Guid.Parse(id));
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                if (id != productEdit.MaHangHoa.ToString())
+                {
+                    return BadRequest();
+                }
+                product.Name = productEdit.Name;
+                product.DonGia = productEdit.DonGia;
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Remove(string id)
+        {
+            try
+            {
+                var product = Products.SingleOrDefault(p => p.MaHangHoa == Guid.Parse(id));
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                Products.Remove(product);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
+
---
+++ WebAPI1/WebAPI1/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace WebAPI1.Models
+{
+    public class ProductVM
+    {
+        public string Name { get; set; }
+        public double DonGia { get; set; }
+    }
+    public class Product:ProductVM
+    {
+        public Guid MaHangHoa { get; set; }
+    }
+}
